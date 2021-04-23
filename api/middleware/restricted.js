42f00c1b3f3ce29fa97f0ae537efc992bd18e9eb
samplesn08 <samplesn08@gmail.com>
2021-04-23 08:34:00 -0400

    token validation middleware complete

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,20 @@
+const jwt = require('jsonwebtoken');
+const { secret } = require('../../data/config/secrets');
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization;
+  if(!token){
+    res.status(401).json({ message: 'token required' })
+  }else{
+    jwt.verify(token, secret, (err, decoded) => {
+      if(err){
+        res.status(401).json({ message: 'token invalid' })
+      }else{
+        req.decodedToken = decoded;
+        next();
+      }
+    })
+  }
   /*
     IMPLEMENT
 
